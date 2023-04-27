cf5f558f1932f7fc91901895b292b067baadfcd3
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-04-27 13:02:32 -0300

    alterando exercício

--- OperadoresAritmeticos/src/main/java/com/mycompany/operadoresaritmeticos/OperadoresAritmeticos.java
+++ OperadoresAritmeticos/src/main/java/com/mycompany/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -12,9 +12,14 @@ package com.mycompany.operadoresaritmeticos;
 public class OperadoresAritmeticos {
 
     public static void main(String[] args) {
-        int n1 = 3;
+        /*int n1 = 3;
         int n2 = 5;
         float media = (n1 + n2)/ 2;
-        System.out.println("A média é igual a " + media);
+        System.out.println("A média é igual a " + media);*/
+        
+        int numero = 5;
+        numero ++;
+        System.out.println(numero);
+                
     }
 }
