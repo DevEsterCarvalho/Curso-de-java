fcd84a7a0808cf78fa78ff7513803e8552f6845d
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-05-05 18:22:03 -0300

    alterando exercício

---
+++ Contador02/src/main/java/com/mycompany/contador02/Contador02.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.contador02;
+
+import contador.TelaContador;
+
+/**
+ *
+ * @author ester
+ */
+public class Contador02 {
+
+    public static void main(String[] args) {
+        new TelaContador().setVisible(true);
+    }
+}
