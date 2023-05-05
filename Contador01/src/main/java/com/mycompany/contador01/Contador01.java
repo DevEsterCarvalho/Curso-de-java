7232c1dcba16be6276e65bc46d227edd0b675276
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-05-05 17:47:26 -0300

    alterando exercício

--- Contador01/src/main/java/com/mycompany/contador01/Contador01.java
+++ Contador01/src/main/java/com/mycompany/contador01/Contador01.java
@@ -12,9 +12,9 @@ package com.mycompany.contador01;
 public class Contador01 {
 
     public static void main(String[] args) {
-        int cc = 0;
-        while (cc < 4) {
-            System.out.println("Cambalhota");
+        int cc = 1;
+        while (cc <= 4) {
+            System.out.println("Cambalhota " + cc);
             cc++;
         }
     }
