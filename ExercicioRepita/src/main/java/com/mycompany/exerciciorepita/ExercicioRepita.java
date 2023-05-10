d6235c9d54a7aac05f14b51dd686722b749b6a38
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-05-10 16:28:24 -0300

    adicionando exercício

---
+++ ExercicioRepita/src/main/java/com/mycompany/exerciciorepita/ExercicioRepita.java
@@ -0,0 +1,19 @@
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.exerciciorepita;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ester
+ */
+public class ExercicioRepita {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello World!", "Boas vindas", JOptionPane.ERROR_MESSAGE);
+    }
+}
