9d53c0ab173f094ae2e7adcc5cfbf7652bd67847
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-05-10 18:42:39 -0300

    alterando exercício

--- ExercicioRepita/src/main/java/com/mycompany/exerciciorepita/ExercicioRepita.java
+++ ExercicioRepita/src/main/java/com/mycompany/exerciciorepita/ExercicioRepita.java
@@ -14,6 +14,41 @@ import javax.swing.JOptionPane;
 public class ExercicioRepita {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello World!", "Boas vindas", JOptionPane.ERROR_MESSAGE);
+        int n, s = 0;
+        int t= 0;
+        int impar = 0;
+        int par = 0;
+        int media = 0;
+        int acima = 0;
+        do {
+            n = Integer.parseInt(JOptionPane.showInputDialog(null, 
+                    "Informe um número: (valor 0 interrompe)"));
+        s += n;
+        
+        t++;
+        
+        if (n % 2 == 1){
+            impar++;
+        } else
+            par++;
+        
+        if (n > 100) {
+            acima++;
+        }
+        
+        media = t/2;
+        
+        }while (n != 0 );
+        JOptionPane.showInputDialog(null, 
+                "<html>Resultado: <br> --------" 
+                    + "<br>Somatório vale " + s 
+                    + "<br>Total de valores : " + t 
+                    + "<br>Total de pares: " + par  
+                    + "<br>Total de impares: " + impar  
+                    + "<br>Acima de 100: " + acima  
+                    + "<br>Média dos valores: " + media
+                    + "<html>",
+                "Resultado final", JOptionPane.WARNING_MESSAGE);
+       
     }
 }
