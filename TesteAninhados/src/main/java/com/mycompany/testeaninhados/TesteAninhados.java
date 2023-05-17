d34986d84e156ef5e7ca66f98cdc733db5a0e198
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-05-17 17:54:41 -0300

    subindo exercício

---
+++ TesteAninhados/src/main/java/com/mycompany/testeaninhados/TesteAninhados.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.testeaninhados;
+
+/**
+ *
+ * @author ester
+ */
+public class TesteAninhados {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <=3; i++) {
+        for (int j = 1; j <=2; j+=2){
+            System.out.println(i + j);
+        }
+    }
+    }
+}
