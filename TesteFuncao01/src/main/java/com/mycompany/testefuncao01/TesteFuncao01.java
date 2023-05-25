3c8e232b20e6d7f6f73c01bb99bc8ac1b6dd7847
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-05-25 14:28:52 -0300

    subindo exercício método/procedimento

---
+++ TesteFuncao01/src/main/java/com/mycompany/testefuncao01/TesteFuncao01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.testefuncao01;
+
+/**
+ *
+ * @author ester
+ */
+public class TesteFuncao01 {
+
+    static void soma (int a, int b){
+        int s = a + b;
+        System.out.println("A soma é : " + s);
+    }
+    public static void main(String[] args) {
+        System.out.println("Começou o programa");
+        soma (5,2);
+    }
+}
