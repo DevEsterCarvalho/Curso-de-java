0060a45cacddbff52311792ebed7cce049a88581
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-05-25 19:00:59 -0300

    subindo testes em java

---
+++ Teste03/src/main/java/com/mycompany/teste03/Teste03.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.teste03;
+
+/**
+ *
+ * @author ester
+ */
+public class Teste03 {
+
+    public static void main(String[] args) {
+        int x, y;
+        double z;
+
+        x = 5;
+        y = 2;
+        z = x / y;
+        System.out.print(z);
+    }
+}
